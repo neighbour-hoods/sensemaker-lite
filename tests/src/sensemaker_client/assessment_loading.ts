3347c47019dc6b1a3304fa516c63746fcabdecc0
pospi <pospi@spadgos.com>
2023-05-03 12:46:01 +1000

    update test to load multiple Assessments initially (which also seems to be failing with BehaviorSubject)

--- tests/src/sensemaker_client/assessment_loading.ts
+++ tests/src/sensemaker_client/assessment_loading.ts
@@ -11,8 +11,10 @@ test('it emits all values progressively loaded into resourceAssessments', async 
 
   const r1 = mockEh()
   const r2 = mockEh()
+  const r3 = mockEh()
   const a1 = mockAssessment({ Integer: 1 }, r1)
   const a2 = mockAssessment({ Integer: 2 }, r2)
+  const a3 = mockAssessment({ Integer: 3 }, r3)
 
   const store = await mockAssessmentsStore({})
 
@@ -22,28 +24,26 @@ test('it emits all values progressively loaded into resourceAssessments', async 
 
   // Load some initial Assessment set from server
 
-  store.mockAssessments({ 'resource_001': [a1] })
-  await store.loadAssessmentsForResources({ resource_ehs: [r1] })
+  store.mockAssessments({ 'resource_001': [a1], 'resource_002': [a2] })
+  await store.loadAssessmentsForResources({ resource_ehs: [r1, r2] })
 
   const expectedMarbles1 = 'a'
-  const expectedValues1 = { a: new Set([a1]) }
+  const expectedValues1 = { a: new Set([a1, a2]) }
 
-  testScheduler.run(({ expectObservable, flush }) => {
+  testScheduler.run(({ expectObservable }) => {
     expectObservable(observed).toBe(expectedMarbles1, expectedValues1)
-    flush()
   })
 
   // Load an additional Assessment set from server, should emit newly returned data
 
-  store.mockAssessments({ 'resource_002': [a2] })
-  await store.loadAssessmentsForResources({ resource_ehs: [r2] })
+  store.mockAssessments({ 'resource_003': [a3] })
+  await store.loadAssessmentsForResources({ resource_ehs: [r3] })
 
   const expectedMarbles2 = 'a'
-  const expectedValues2 = { a: new Set([a1, a2]) }
+  const expectedValues2 = { a: new Set([a1, a2, a3]) }
 
-  testScheduler.run(({ expectObservable, flush }) => {
+  testScheduler.run(({ expectObservable }) => {
     expectObservable(observed).toBe(expectedMarbles2, expectedValues2)
-    flush()
   })
 
   // Newly bound observers to the assessments should get them all
@@ -51,10 +51,9 @@ test('it emits all values progressively loaded into resourceAssessments', async 
   const newlyObserved = store.resourceAssessments()
 
   const expectedMarbles3 = 'a'
-  const expectedValues3 = { a: new Set([a1, a2]) }
+  const expectedValues3 = { a: new Set([a1, a2, a3]) }
 
-  testScheduler.run(({ expectObservable, flush }) => {
+  testScheduler.run(({ expectObservable }) => {
     expectObservable(newlyObserved).toBe(expectedMarbles3, expectedValues3)
-    flush()
   })
 })
