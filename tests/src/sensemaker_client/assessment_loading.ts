07a3605974030efacdc5afd6e2c27d14584627b3
pospi <pospi@spadgos.com>
2023-05-06 08:28:33 +1000

    add tests for Assessment filtering

--- tests/src/sensemaker_client/assessment_loading.ts
+++ tests/src/sensemaker_client/assessment_loading.ts
@@ -3,6 +3,9 @@ import { of, filter, lastValueFrom } from 'rxjs'
 
 import { scheduler, mockAssessmentsStore, mockAssessment, mockEh } from '../store_mocks'
 import { Assessment } from '@neighbourhoods/client'
+import { encodeHashToBase64 } from "@holochain/client"
+
+
 
 test('it emits all values progressively loaded into resourceAssessments', async (t) => {
   const testScheduler = scheduler(t)
@@ -55,5 +58,54 @@ test('it emits all values progressively loaded into resourceAssessments', async 
 
   testScheduler.run(({ expectObservable }) => {
     expectObservable(newlyObserved).toBe(expectedMarbles3, expectedValues3)
+  })
+})
+
+
+
+test('it emits a filtered stream of resourceAssessments based on matching dimensionEh', async (t) => {
+  const testScheduler = scheduler(t)
+
+  // configure mock data
+
+  const d1 = mockEh(), d2 = mockEh()
+  const r1 = mockEh()
+  const a1 = mockAssessment({ Integer: 1 }, r1, d1),
+    a2 = mockAssessment({ Integer: 2 }, 0, d2),
+    a3 = mockAssessment({ Integer: 3 }, 0, d1),
+    a4 = mockAssessment({ Integer: 2 }),
+    a5 = mockAssessment({ Integer: 4 }, r1)
+
+  const store = await mockAssessmentsStore({
+    'resource_001': [a1, a5],
+    'resource_002': [a2, a3, a4]
+  })
+
+  // START TEST LOGIC
+
+  const observing1 = store.resourceAssessments({ dimensionEhs: [encodeHashToBase64(d1)] })
+  const observing2 = store.resourceAssessments({ dimensionEhs: [encodeHashToBase64(d1), encodeHashToBase64(d2)] })
+  const observingR1 = store.resourceAssessments({ resourceEhs: [encodeHashToBase64(r1)] })
+  const observingR1D1 = store.resourceAssessments({ resourceEhs: [encodeHashToBase64(r1)], dimensionEhs: [encodeHashToBase64(d1)] })
+
+  await store.loadAssessmentsForResources({})
+
+  // filtering works as expected
+
+  testScheduler.run(({ expectObservable }) => {
+    expectObservable(observing1).toBe('a', { a: new Set([a1, a3]) })
+    expectObservable(observing2).toBe('a', { a: new Set([a1, a2, a3]) })
+    expectObservable(observingR1).toBe('a', { a: new Set([a1, a5]) })
+    expectObservable(observingR1D1).toBe('a', { a: new Set([a1]) })
+  })
+
+  // late subscribers get existing data
+
+  const observing3 = store.resourceAssessments({ dimensionEhs: [encodeHashToBase64(d1)] })
+  const observing4 = store.resourceAssessments({ dimensionEhs: [encodeHashToBase64(d1), encodeHashToBase64(d2)] })
+
+  testScheduler.run(({ expectObservable }) => {
+    expectObservable(observing3).toBe('a', { a: new Set([a1, a3]) })
+    expectObservable(observing4).toBe('a', { a: new Set([a1, a2, a3]) })
   })
 })
